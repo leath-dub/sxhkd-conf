0db03ef0d7f7122d3b04c603a69742253fdd2118
leath-dub <fierceinbattle@gmail.com>
2023-10-30 19:44:47 +0000

    silly me

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-dir.url = "github:leath-dub/nix-dir";
     systems.url = "github:nix-systems/default";
   };
 
@@ -12,10 +11,7 @@
         nixpkgs.lib.genAttrs (import systems) (system: fn nixpkgs.legacyPackages.${system} system);
     in {
       packages = forAllSystems (pkgs: system: {
-        default = nix-dir.lib.rawDir {
-          inherit pkgs system;
-          src = ./.;
-        };
+        default = "${./.}"
       });
     };
 }
