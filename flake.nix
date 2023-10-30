6633840b87357f586708dfade43cf539203fc825
leath-dub <fierceinbattle@gmail.com>
2023-10-30 19:46:54 +0000

    silly me

--- flake.nix
+++ flake.nix
@@ -6,12 +6,12 @@
     systems.url = "github:nix-systems/default";
   };
 
-  outputs = { self, nixpkgs, nix-dir, systems }: let
+  outputs = { self, nixpkgs, systems }: let
       forAllSystems = fn:
         nixpkgs.lib.genAttrs (import systems) (system: fn nixpkgs.legacyPackages.${system} system);
     in {
       packages = forAllSystems (pkgs: system: {
-        default = "${./.}"
+        default = "${./.}";
       });
     };
 }
