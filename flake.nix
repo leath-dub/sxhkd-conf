008210074f7e6bf2a1f39499749536480a904410
leath-dub <fierceinbattle@gmail.com>
2023-10-30 19:48:46 +0000

    even simpler

--- flake.nix
+++ flake.nix
@@ -1,17 +1,8 @@
 {
   description = "My neovim config";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    systems.url = "github:nix-systems/default";
-  };
-
-  outputs = { self, nixpkgs, systems }: let
-      forAllSystems = fn:
-        nixpkgs.lib.genAttrs (import systems) (system: fn nixpkgs.legacyPackages.${system} system);
+  outputs = { self }: let
     in {
-      packages = forAllSystems (pkgs: system: {
-        default = "${./.}";
-      });
+      src = "${./.}";
     };
 }
